5f826e19b618a9d7133815bc26b4ae1bc75a1c56
Prem Nalla <premnalla@gmail.com>
2024-03-15 16:24:37 -0400

    Updated provider for ClusterControl - Making ready for TF registry

--- examples/replication/mysql80/aws/getmyip.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Fetch the public IP address and return it in JSON format
-IP=$(curl -s https://ifconfig.me)
-echo "{\"ip\":\"${IP}\"}"
-
--- examples/replication/mysql80/aws/version.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    cc = {
-      source = "severalnines.com/severalnines/clustercontrol"
-      version = ">= 0.0.1"
-    }
-  }
-}
