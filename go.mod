209b1b8fb96e2a5b8af28d4e3d6289b99991486b
Prem Nalla <premnalla@gmail.com>
2024-04-29 11:44:31 -0400

    fixed pointing to old repo

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/premnalla/new-terraform-provider-clustercontrol
+module github.com/severalnines/terraform-provider-clustercontrol
 
 go 1.21.6
 
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/premnalla/new-terraform-provider-clustercontrol/internal/provider"
+	"github.com/severalnines/terraform-provider-clustercontrol/internal/provider"
 )
 
 func main() {
